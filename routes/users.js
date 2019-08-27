ae59968b1755f73199a69b6eca39adca48cd5273
Vladimir Parfenov <vparfenov@ptc.com>
2019-08-26 22:49:27 -0400

    Debug auth

--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,18 @@ const sessionDb = require ('../app/init').sessionDb
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  
   const users = sessionDb.listUsers ().map (user => {
-    return {username: user.username, models: user.listModels()}
+    return {username: user.username, models: user.listModels(), principal: req.headers ['x-ms-client-principal-name']}
   })
-  res.render('users', { title: 'Students', users });
+  let headers = req.headers
+  res.render('users', {
+    title: 'Students',
+    users,
+    headers: Object.keys (headers).map (key => {
+      return {key, value: headers [key]}
+    })
+  });
 });
 
 module.exports = router;
