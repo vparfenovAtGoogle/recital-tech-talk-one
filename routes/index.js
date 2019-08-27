970b6ce902297e72abb23680c566a97b8f5d5468
Vladimir Parfenov <vparfenov@ptc.com>
2019-08-27 15:51:29 -0400

    Default re directions from index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const googleAuthSecret = process.env ['WEBSITE_AUTH_GOOGLE_CLIENT_SECRET']
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'PTC Tech Talk!' });
+  const userAgent = req.headers ['user-agent']
+  const principal = req.query.principal || req.headers ['x-ms-client-principal-name']
+  if (principal) {
+    res.redirect ('/users')
+  }
+  else if (userAgent && userAgent.indexOf ('Creo') > -1) {
+    res.redirect ('/creo_app.html')
+  }
+  else if (googleAuthSecret) {
+    res.redirect ('/.auth/login/google?post_login_redirect_url=/users')
+  }
+  else {
+    res.render('index', { title: 'PTC Tech Talk!' });
+  }
 });
 
 module.exports = router;
